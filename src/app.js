ff72cbaab05bbe1f77d001f0064e449145891678
BrunoGodoy07 <48246289@est.ort.edu.ar>
2025-04-28 08:40:05 -0300

    tp finalizado (ahora si xd)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import { horaActual } from "./time";
-
+import { horaActual } from "./time.js";
+import  express  from "express";
 const app = express();
 
 app.get('/', (req,res) =>{
@@ -13,12 +13,10 @@ app.get('/hora', (req,res) =>{
 })
 //http://localhost:3000
 
-app.get('/hora', (req,res) =>{
-    let hora = horaActual();
-    res.send(`La hora actual es: ${hora}`);
+app.get('/fecha-completa', (req,res) =>{
+    let fecha = new Date();
+    res.send(`La fecha completa es: ${fecha.toLocaleString()}`);
 })
 //http://localhost:3000
 
-app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`)
-})
+export default app
